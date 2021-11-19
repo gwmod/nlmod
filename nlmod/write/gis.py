d541086ebdb0a3fe5666b37280a57bc0c75291dd
OnnoEbbens <onnoebbens@gmail.com>
2021-11-19 11:00:26 +0100

    add functionality for structured grids

--- nlmod/write/gis.py
+++ nlmod/write/gis.py
@@ -14,14 +14,56 @@ To-do:
 import os
 from shapely.geometry import Polygon
 import geopandas as gpd
+import numpy as np
 import logging
 
 logger = logging.getLogger(__name__)
 
 
-def dataarray_to_gdf(model_ds, data_variables, polygons=None,
-                     dealing_with_time='mean'):
-    """ Convert one or more DataArrays from a model dataset to a 
+def _polygons_from_model_ds(model_ds):
+    """ create polygons of each cell in a model dataset
+
+    Parameters
+    ----------
+    model_ds : xr.DataSet
+        xarray with model data
+
+    Raises
+    ------
+    ValueError
+        for wrong gridtype or inconsistent grid definition.
+
+    Returns
+    -------
+    polygons : list of shapely Polygons
+        list with polygon of each raster cell.
+
+    """
+    
+    if model_ds.gridtype == 'structured':
+        # check if coördinates are consistent with delr/delc values
+        delr_x = np.unique(model_ds.x.values[1:] - model_ds.x.values[:-1])
+        delc_y = np.unique(model_ds.y.values[:-1] - model_ds.y.values[1:])
+        if not ((delr_x == model_ds.delr) and (delc_y == model_ds.delc)):
+            raise ValueError('delr and delc attributes of model_ds inconsistent with x and y coördinates')
+            
+        xmins = model_ds.x - (model_ds.delr*0.5)
+        xmaxs = model_ds.x + (model_ds.delr*0.5)
+        ymins = model_ds.y - (model_ds.delc*0.5)
+        ymaxs = model_ds.y + (model_ds.delc*0.5)
+        polygons = [Polygon([(xmins[i], ymins[j]), (xmins[i], ymaxs[j]), (xmaxs[i], ymaxs[j]), (xmaxs[i], ymins[j])]) for i in range(len(xmins)) for j in range(len(ymins))]
+    elif model_ds.gridtype == 'unstructured':
+        polygons = [Polygon(vertices) for vertices in model_ds['vertices'].values]
+    else:
+        raise ValueError(f"gridtype must be 'structured' or 'unstructured', not f{model_ds.gridtype}")
+        
+    return polygons
+
+
+
+def vertex_dataarray_to_gdf(model_ds, data_variables, polygons=None,
+                            dealing_with_time='mean'):
+    """ Convert one or more DataArrays from a vertex model dataset to a 
     Geodataframe.
 
 
@@ -35,6 +77,11 @@ def dataarray_to_gdf(model_ds, data_variables, polygons=None,
     polygons : list of shapely Polygons, optional
         geometries used for the GeoDataframe, if None the polygons are created
         from the data variable 'vertices' in model_ds. The default is None.
+    dealing_with_time : str, optional
+        when there is time variant data in the model dataset this function
+        becomes very slow. For now only the time averaged data will be 
+        saved in the geodataframe. Later this can be extended with multiple 
+        possibilities. The default is 'mean'.
 
     Raises
     ------
@@ -75,8 +122,74 @@ def dataarray_to_gdf(model_ds, data_variables, polygons=None,
 
     # create geometries
     if polygons is None:
-        polygons = [Polygon(vertices) for vertices in model_ds['vertices'].values]
+        polygons = _polygons_from_model_ds(model_ds)
 
+    # construct geodataframe
+    gdf = gpd.GeoDataFrame(dv_dic, geometry=polygons)
+
+    return gdf
+
+
+
+
+def struc_dataarray_to_gdf(model_ds, data_variables, polygons=None,
+                           dealing_with_time='mean'):
+    """ Convert one or more DataArrays from a structured model dataset to a 
+    Geodataframe.
+
+
+    Parameters
+    ----------
+    model_ds : xr.DataSet
+        xarray with model data
+    data_variables : list, tuple or set
+        data_variables in model_ds that will be stored as attributes in the
+        shapefile.
+    polygons : list of shapely Polygons, optional
+        geometries used for the GeoDataframe, if None the polygons are created
+        from the data variable 'vertices' in model_ds. The default is None.
+
+    Raises
+    ------
+    ValueError
+        for DataArrays with unexpected dimension names.
+    NotImplementedError
+        for DataArrays with more than 2 dimensions or dealing_with_time!='mean'
+
+    Returns
+    -------
+    gdf : geopandas.GeoDataframe
+        geodataframe of one or more DataArrays.
+
+    """
+    assert model_ds.gridtype == 'structured', f'expected model dataset with gridtype unstructured, got {model_ds.gridtype}'
+
+    # create dictionary with column names and values of the geodataframe
+    dv_dic = {}
+    for da_name in data_variables:
+        da = model_ds[da_name]
+        no_dims = len(da.dims)
+        if no_dims == 2:
+            dv_dic[da_name] = da.values.flatten('F')
+        elif no_dims == 3:
+            if da.dims == ('layer','y','x'):
+                for i, da_lay in enumerate(da):
+                    dv_dic[f'{da_name}_lay{i}'] = da_lay.values.flatten('F')
+            elif 'time' in da.dims:
+                da_mean = da.mean(dim='time')
+                if dealing_with_time == 'mean':
+                    dv_dic[f'{da_name}_mean'] = da_mean.values.flatten('F')
+                else:
+                    raise NotImplementedError("Can only use the mean of a DataArray with dimension time, use dealing_with_time='mean'")
+            else:
+                raise ValueError(f"expected dimensions ('layer', 'y', 'x'), got {da.dims}")
+        else:
+            raise NotImplementedError(f'expected two or three dimensions got {no_dims}')
+    
+    # create geometries
+    if polygons is None:
+        polygons = _polygons_from_model_ds(model_ds)
+        
     # construct geodataframe
     gdf = gpd.GeoDataFrame(dv_dic, geometry=polygons)
 
@@ -107,7 +220,12 @@ def dataarray_to_shapefile(model_ds, data_variables, fname, polygons=None):
     None.
 
     """
-    gdf = dataarray_to_gdf(model_ds, data_variables, polygons=polygons)
+    if model_ds.gridtype == 'unstructured':
+        gdf = vertex_dataarray_to_gdf(model_ds, data_variables, 
+                                      polygons=polygons)
+    else:
+        gdf = struc_dataarray_to_gdf(model_ds, data_variables, 
+                                     polygons=polygons)
     gdf.to_file(fname)
 
 
@@ -141,8 +259,8 @@ def model_dataset_to_vector_file(model_ds,
 
     Returns
     -------
-    fnames : list of str
-        filenames of exported shapefiles.
+    fnames : str or list of str
+        filename(s) of exported geopackage or shapefiles.
 
     """
 
@@ -173,12 +291,15 @@ def model_dataset_to_vector_file(model_ds,
     da_names -= set(exclude)
 
     # create list of polygons
-    polygons = [Polygon(vertices) for vertices in model_ds['vertices'].values]
+    polygons = _polygons_from_model_ds(model_ds)
 
     # combine some data variables in one shapefile
     for key, item in combine_dic.items():
         if set(item).issubset(da_names):
-            gdf = dataarray_to_gdf(model_ds, item, polygons=polygons)
+            if model_ds.gridtype =='structured':
+                gdf = struc_dataarray_to_gdf(model_ds, item, polygons=polygons)
+            elif model_ds.gridtype =='unstructured':
+                gdf = vertex_dataarray_to_gdf(model_ds, item, polygons=polygons)
             if driver == 'GPKG':
                 gdf.to_file(fname_gpkg, layer=key, driver=driver)
             else:
@@ -191,7 +312,10 @@ def model_dataset_to_vector_file(model_ds,
 
     # create unique shapefiles for the other data variables
     for da_name in da_names:
-        gdf = dataarray_to_gdf(model_ds, (da_name,), polygons=polygons)
+        if model_ds.gridtype =='structured':
+            gdf = struc_dataarray_to_gdf(model_ds, (da_name,), polygons=polygons)
+        elif model_ds.gridtype =='unstructured':
+            gdf = vertex_dataarray_to_gdf(model_ds, (da_name,), polygons=polygons)
         if driver == 'GPKG':
             gdf.to_file(fname_gpkg, layer=da_name, driver=driver)
         else:
@@ -199,6 +323,7 @@ def model_dataset_to_vector_file(model_ds,
             gdf.to_file(fname, driver=driver)
             fnames.append(fname)
 
+    # return filename(s)
     if driver == 'GPKG':
         return fname_gpkg
     else:

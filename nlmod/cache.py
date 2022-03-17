3ff582793d3b051e6bee4ee4b893fe10249aa824
OnnoEbbens <onnoebbens@gmail.com>
2022-03-17 14:33:49 +0100

    add check for mf2005 model

--- nlmod/cache.py
+++ nlmod/cache.py
@@ -205,7 +205,7 @@ def cache_pklz(func):
         if not cachename.endswith('.pklz'):
             cachename += '.pklz'
 
-        fname_cache = os.path.join(cachedir, cachename)  # netcdf file
+        fname_cache = os.path.join(cachedir, cachename)  # pklz file
         fname_args_cache = fname_cache.replace(
             '.pklz', '_cache.pklz')  # pickle with function arguments
 
@@ -353,7 +353,7 @@ def _same_function_arguments(func_args_dic, func_args_dic_cache):
                 logger.info(
                     'cache was created using different dictionaries, do not use cached data')
                 return False
-        elif isinstance(item, flopy.mf6.ModflowGwf):
+        elif isinstance(item, (flopy.mf6.ModflowGwf, flopy.modflow.mf.Modflow)):
             if str(item) != str(func_args_dic_cache[key]):
                 logger.info(
                     'cache was created using different groundwater flow model, do not use cached data')

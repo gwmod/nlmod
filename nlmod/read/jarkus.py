228b179fa21fbcda3d3ad018ca33bebe6b4edd42
Ruben Caljé <r.calje@artesia-water.nl>
2022-05-02 10:47:04 +0200

    fix test for northsea

--- nlmod/read/jarkus.py
+++ nlmod/read/jarkus.py
@@ -45,7 +45,9 @@ def get_bathymetry(model_ds, northsea):
 
     # no bathymetry if we don't have northsea
     if (northsea == 0).all():
-        model_ds_out['bathymetry'] = northsea.dims, np.nan
+        model_ds_out['bathymetry'] = util.get_da_from_da_ds(northsea,
+                                                            northsea.dims,
+                                                            data=np.nan)
         return model_ds_out
 
     # try to get bathymetry via opendap

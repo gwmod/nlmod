490b79d4fbbfaf3ddac613e502ae113fa5d50b28
Ruben Caljé <r.calje@artesia-water.nl>
2022-08-17 11:42:24 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -85,3 +85,6 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+# Allow errors in notebooks, so we can see the error online
+nbsphinx_allow_errors = True

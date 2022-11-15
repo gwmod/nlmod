8f66bdc07af2798c890cc81222fcc22c05080192
Ruben Caljé <r.calje@artesia-water.nl>
2022-11-15 14:29:54 +0100

    Update output.py

--- nlmod/gwf/output.py
+++ nlmod/gwf/output.py
@@ -34,32 +34,14 @@ def get_heads_da(ds=None, gwf=None, fname_hds=None):
     head_ar : xarray.DataArray
         heads array.
     """
-    assert (
-        (ds is not None) + (gwf is not None)
-    ) == 1, "Load the heads using either the ds or the gwf"
+    headobj = _get_hds(ds=ds, gwf=gwf, fname_hds=fname_hds)
 
-    if fname_hds is not None and ds is not None:
-        headobj = flopy.utils.HeadFile(fname_hds)
-        hdry = -1e30
-        hnoflo = 1e30
-
-    elif fname_hds is not None and gwf is not None:
-        headobj = flopy.utils.HeadFile(fname_hds)
+    if gwf is not None:
         hdry = gwf.hdry
         hnoflo = gwf.hnoflo
-
-    elif fname_hds is None and gwf is not None:
-        headobj = gwf.output.head()
-        hdry = gwf.hdry
-        hnoflo = gwf.hnoflo
-
-    elif fname_hds is None and ds is not None:
-        fname_hds = os.path.join(ds.model_ws, ds.model_name + ".hds")
-        headobj = flopy.utils.HeadFile(fname_hds)
-        hdry = -1e30
-        hnoflo = 1e30
     else:
-        pass
+        hdry = -1e30
+        hnoflo = 1e30
 
     heads = headobj.get_alldata()
     heads[heads == hdry] = np.nan
@@ -132,9 +114,83 @@ def get_heads_da(ds=None, gwf=None, fname_hds=None):
         affine = get_affine(attrs)
         head_ar.rio.write_transform(affine, inplace=True)
 
-    else:
-        pass
-
     head_ar.rio.write_crs("EPSG:28992", inplace=True)
 
     return head_ar
+
+
+def _get_hds(ds=None, gwf=None, fname_hds=None):
+    msg = "Load the heads using either the ds or the gwf"
+    assert ((ds is not None) + (gwf is not None)) == 1, msg
+
+    if fname_hds is None:
+        if ds is None:
+            headobj = gwf.output.head()
+        else:
+            fname_hds = os.path.join(ds.model_ws, ds.model_name + ".hds")
+    if fname_hds is not None:
+        headobj = flopy.utils.HeadFile(fname_hds)
+    return headobj
+
+
+def _get_cbc(ds=None, gwf=None, fname_cbc=None):
+    msg = "Load the budgets using either the ds or the gwf"
+    assert ((ds is not None) + (gwf is not None)) == 1, msg
+
+    if fname_cbc is None:
+        if ds is None:
+            cbf = gwf.output.budget()
+        else:
+            fname_cbc = os.path.join(ds.model_ws, ds.model_name + ".cbc")
+    if fname_cbc is not None:
+        cbf = flopy.utils.CellBudgetFile(fname_cbc)
+    return cbf
+
+
+def get_gwl_from_wet_cells(head, layer="layer", botm=None):
+    """
+    Get the groundwater level from a multi-dimensional head array where dry
+    cells are NaN. This methods finds the most upper non-nan-value of each cell
+    or timestep.
+
+    Parameters
+    ----------
+    head : xarray.DataArray or numpy array
+        A multi-dimensional array of head values. NaN-values represent inactive
+        or dry cells.
+    layer : string or int, optional
+        The name of the layer dimension of head (if head is a DataArray) or the integer
+        of the layer dimsension of head (if head is a numpy array). The default is
+        'layer'.
+    botm : xarray.DataArray, optional
+        A DataArray with the botm of each model-cell. It can be used to set heads below
+        the botm of the cells to NaN. botm is only used when head is a DataArray.
+
+    Returns
+    -------
+    gwl : numpy-array
+        An array of the groundwater-level, without the layer-dimension.
+
+    """
+    if isinstance(head, xr.DataArray):
+        head_da = head
+        if botm is not None:
+            head_da = head_da.where(head_da > botm)
+        head = head_da.data
+        if isinstance(layer, str):
+            layer = head_da.dims.index(layer)
+    else:
+        head_da = None
+    # take the first non-nan value along the layer dimension (1)
+    top_layer = np.expand_dims(np.isnan(head).argmin(layer), layer)
+    gwl = np.take_along_axis(head, top_layer, axis=layer)
+    gwl = np.take(gwl, 0, axis=layer)
+    if head_da is not None:
+        dims = list(head_da.dims)
+        dims.pop(layer)
+        coords = dict(head_da.coords)
+        # store the layer in which the groundwater level is of each cell and time
+        top_layer = np.take(top_layer, 0, axis=layer)
+        coords["layer"] = (dims, head_da.layer.data[top_layer])
+        gwl = xr.DataArray(gwl, dims=dims, coords=coords)
+    return gwl

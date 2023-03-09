c364c13e9739ff890d33f2604b124f91382318e1
dbrakenhoff <d.brakenhoff@artesia-water.nl>
2023-03-09 10:57:36 +0100

    improve gwt - reuse gwf dis/disv and _set_record - improve getting vars from dataset or passing directly to function - for ssm, get sources from ssm_sources attribute (auto added when gwf pkgs are created with aux vars)

--- nlmod/gwt/__init__.py
+++ nlmod/gwt/__init__.py
@@ -1,1 +1,4 @@
+from . import output, prepare
 from .gwt import *
+from .output import *
+from .prepare import *
--- nlmod/gwt/gwt.py
+++ nlmod/gwt/gwt.py
@@ -6,6 +6,7 @@ import numpy as np
 import xarray as xr
 
 from ..dims import grid
+from ..gwf.gwf import _set_record, dis, disv
 
 logger = logging.getLogger(__name__)
 
@@ -32,136 +33,16 @@ def gwt(ds, sim, **kwargs):
     logger.info("creating modflow GWT")
 
     # Create the Flopy groundwater flow (gwf) model object
-    model_nam_file = f"gwt_{ds.model_name}.nam"
+    model_nam_file = f"{ds.model_name}_gwt.nam"
 
     gwt = flopy.mf6.ModflowGwt(
-        sim, modelname=f"gwt_{ds.model_name}", model_nam_file=model_nam_file, **kwargs
+        sim, modelname=f"{ds.model_name}_gwt", model_nam_file=model_nam_file, **kwargs
     )
 
     return gwt
 
 
-def dis(ds, gwt, length_units="METERS", pname="dis", **kwargs):
-    """get discretisation package from the model dataset.
-
-    Parameters
-    ----------
-    ds : xarray.Dataset
-        dataset with model data.
-    gwt : flopy ModflowGwf
-        groundwater transport object.
-    length_units : str, optional
-        length unit. The default is 'METERS'.
-    pname : str, optional
-        package name
-
-    Returns
-    -------
-    dis : TYPE
-        discretisation package.
-    """
-    logger.info("creating modflow DIS")
-
-    if ds.gridtype == "vertex":
-        return disv(ds, gwt, length_units=length_units)
-
-    # check attributes
-    for att in ["delr", "delc"]:
-        if att in ds.attrs:
-            if isinstance(ds.attrs[att], np.float32):
-                ds.attrs[att] = float(ds.attrs[att])
-
-    if "angrot" in ds.attrs and ds.attrs["angrot"] != 0.0:
-        xorigin = ds.attrs["xorigin"]
-        yorigin = ds.attrs["yorigin"]
-        angrot = ds.attrs["angrot"]
-    else:
-        xorigin = ds.extent[0]
-        yorigin = ds.extent[2]
-        angrot = 0.0
-
-    dis = flopy.mf6.ModflowGwtdis(
-        gwt,
-        pname=pname,
-        length_units=length_units,
-        xorigin=xorigin,
-        yorigin=yorigin,
-        angrot=angrot,
-        nlay=ds.dims["layer"],
-        nrow=ds.dims["y"],
-        ncol=ds.dims["x"],
-        delr=ds["delr"].values if "delr" in ds else ds.delr,
-        delc=ds["delc"].values if "delc" in ds else ds.delc,
-        top=ds["top"].data,
-        botm=ds["botm"].data,
-        idomain=ds["idomain"].data,
-        filename=f"gwt_{ds.model_name}.dis",
-        **kwargs,
-    )
-
-    return dis
-
-
-def disv(ds, gwt, length_units="METERS", pname="disv", **kwargs):
-    """create transport discretisation vertices package from the model dataset.
-
-    Parameters
-    ----------
-    ds : xarray.Dataset
-        dataset with model data.
-    gwt : flopy ModflowGwt
-        groundwater transport object.
-    length_units : str, optional
-        length unit. The default is 'METERS'.
-    pname : str, optional
-        package name
-
-    Returns
-    -------
-    disv : flopy ModflowGwtdisv
-        disv package
-    """
-    logger.info("creating modflow DISV")
-
-    if "angrot" in ds.attrs and ds.attrs["angrot"] != 0.0:
-        xorigin = ds.attrs["xorigin"]
-        yorigin = ds.attrs["yorigin"]
-        angrot = ds.attrs["angrot"]
-    elif "extent" in ds.attrs.keys():
-        xorigin = ds.attrs["extent"][0]
-        yorigin = ds.attrs["extent"][2]
-        angrot = 0.0
-    else:
-        xorigin = 0.0
-        yorigin = 0.0
-        angrot = 0.0
-
-    vertices = grid.get_vertices_from_ds(ds)
-    cell2d = grid.get_cell2d_from_ds(ds)
-    disv = flopy.mf6.ModflowGwtdisv(
-        gwt,
-        idomain=ds["idomain"].data,
-        xorigin=xorigin,
-        yorigin=yorigin,
-        length_units=length_units,
-        angrot=angrot,
-        nlay=len(ds.layer),
-        ncpl=len(ds.icell2d),
-        nvert=len(ds.iv),
-        top=ds["top"].data,
-        botm=ds["botm"].data,
-        vertices=vertices,
-        cell2d=cell2d,
-        pname=pname,
-        **kwargs,
-    )
-    if "angrot" in ds.attrs and ds.attrs["angrot"] != 0.0:
-        gwt.modelgrid.set_coord_info(xoff=xorigin, yoff=yorigin, angrot=angrot)
-
-    return disv
-
-
-def adv(ds, gwt, scheme="UPSTREAM", **kwargs):
+def adv(ds, gwt, scheme=None, **kwargs):
     """create advection package for groundwater transport model.
 
     Parameters
@@ -179,37 +60,59 @@ def adv(ds, gwt, scheme="UPSTREAM", **kwargs):
     adv : flopy ModflowGwtadv
         adv package
     """
-    ds.attrs["advection_scheme"] = scheme
+    logger.info("creating modflow ADV")
+    if scheme is None:
+        scheme = ds.attrs.get("adv_scheme")
+    else:
+        ds.attrs["adv_scheme"] = scheme
     adv = flopy.mf6.ModflowGwtadv(gwt, scheme=scheme, **kwargs)
     return adv
 
 
 def dsp(ds, gwt, **kwargs):
-    alpha_l = 1.0  # Longitudinal dispersivity ($m$)
-    alpha_th = 0.1  # Transverse horizontal dispersivity ($m$)
-    alpha_tv = 0.1  # Transverse vertical dispersivity ($m$)
-    dsp = flopy.mf6.ModflowGwtdsp(
-        gwt, xt3d_off=True, alh=alpha_l, ath1=alpha_th, atv=alpha_tv
-    )
+    logger.info("creating modflow DSP")
+
+    alh = kwargs.pop("alh", ds.dsp_alh)
+    ath1 = kwargs.pop("ath1", ds.dsp_ath1)
+    atv = kwargs.pop("atv", ds.dsp_atv)
+
+    if "alh" not in kwargs:
+        ds.attrs["dsp_alh"] = alh
+    if "ath1" not in kwargs:
+        ds.attrs["dsp_ath1"] = ath1
+    if "atv" not in kwargs:
+        ds.attrs["dsp_atv"] = atv
+
+    dsp = flopy.mf6.ModflowGwtdsp(gwt, alh=alh, ath1=ath1, atv=atv, **kwargs)
     return dsp
 
 
-def ssm(ds, gwt, pkg_sources=None, **kwargs):
+def ssm(ds, gwt, pkg_sources=None, sources=None, **kwargs):
     logger.info("creating modflow SSM")
 
-    if pkg_sources is not None:
+    if pkg_sources is not None and sources is None:
         sources = [(ipkg, "AUX", "CONCENTRATION") for ipkg in pkg_sources]
-    else:
-        sources = kwargs.pop("sources")
+        ds.attrs["ssm_sources"] = pkg_sources
+    elif sources is None:
+        sources = [
+            (ipkg.upper(), "AUX", "CONCENTRATION") for ipkg in ds.attrs["ssm_sources"]
+        ]
 
     ssm = flopy.mf6.ModflowGwtssm(gwt, sources=sources, **kwargs)
     return ssm
 
 
-def mst(ds, gwt, **kwargs):
+def mst(ds, gwt, porosity=None, **kwargs):
     logger.info("creating modflow MST")
 
-    porosity = kwargs.get("porosity", ds.porosity)
+    if porosity is None:
+        porosity = ds.porosity
+    else:
+        if isinstance(porosity, float):
+            ds.attrs["porosity"] = porosity
+        else:
+            logger.warn("the porosity passed to mst pkg is not stored in ds")
+
     mst = flopy.mf6.ModflowGwtmst(gwt, porosity=porosity)
     return mst
 
@@ -225,25 +128,6 @@ def cnc(ds, gwt, da_mask, da_conc, pname="cnc", **kwargs):
     return cnc
 
 
-def _set_record(conc, budget):
-    record = []
-    if isinstance(conc, bool):
-        if conc:
-            conc = "LAST"
-        else:
-            conc = None
-    if conc is not None:
-        record.append(("CONCENTRATION", conc))
-    if isinstance(budget, bool):
-        if budget:
-            budget = "LAST"
-        else:
-            budget = None
-    if budget is not None:
-        record.append(("BUDGET", budget))
-    return record
-
-
 def oc(
     ds,
     gwt,
@@ -257,12 +141,12 @@ def oc(
     logger.info("creating modflow OC")
 
     # Create the output control package
-    concfile = f"gwt_{ds.model_name}.ucn"
+    concfile = f"{gwt.name}.ucn"
     conc_filerecord = [concfile]
-    budgetfile = f"gwt_{ds.model_name}.cbc"
+    budgetfile = f"{gwt.name}.cbc"
     budget_filerecord = [budgetfile]
-    saverecord = _set_record(save_concentration, save_budget)
-    printrecord = _set_record(print_concentration, print_budget)
+    saverecord = _set_record(save_concentration, save_budget, output="concentration")
+    printrecord = _set_record(print_concentration, print_budget, output="concentration")
 
     oc = flopy.mf6.ModflowGwtoc(
         gwt,
@@ -312,8 +196,10 @@ def ic(ds, gwt, strt="chloride", pname="ic", **kwargs):
 
 
 def gwfgwt(ds, sim, exgtype="GWF6-GWT6", **kwargs):
+    logger.info("creating modflow exchange GWFGWT")
+
     exgnamea = ds.model_name
-    exgnameb = f"gwt_{ds.model_name}"
+    exgnameb = f"{ds.model_name}_gwt"
     # exchange
     gwfgwt = flopy.mf6.ModflowGwfgwt(
         sim,

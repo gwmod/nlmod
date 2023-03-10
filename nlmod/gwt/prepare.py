e0dfdc8b8a17d4508026cfc35885ce69bdd3b7b7
dbrakenhoff <d.brakenhoff@artesia-water.nl>
2023-03-10 12:03:47 +0100

    docstring

--- nlmod/gwt/prepare.py
+++ nlmod/gwt/prepare.py
@@ -1,4 +1,21 @@
 def set_default_transport_parameters(ds, transport_type):
+    """Set default transport parameters based on type of transport model.
+
+    Convenience function for setting several variables at once for which
+    default values are often used.
+
+    Parameters
+    ----------
+    ds : xarray.Dataset
+        dataset
+    transport_type : str
+        type of transport model, currently supports "chloride" or "tracer".
+
+    Returns
+    -------
+    ds : xarray.Dataset
+        dataset with transport parameters added to attributes.
+    """
     if transport_type == "chloride":
         # buy
         ds.attrs["drhodc"] = 25.0 / 18_000.0  # delta density / delta concentration

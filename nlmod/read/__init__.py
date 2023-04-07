8341c61bdca428eaf09fe2114c89ad2ad29edf24
Martin Vonk <vonk.mart@gmail.com>
2023-04-07 12:39:15 +0200

    Update __init__.py
    add meteobase

--- nlmod/read/__init__.py
+++ nlmod/read/__init__.py
@@ -1,2 +1,14 @@
-from . import ahn, bgt, brp, geotop, jarkus, knmi, regis, rws, waterboard, webservices
+from . import (
+    ahn,
+    bgt,
+    brp,
+    geotop,
+    jarkus,
+    knmi,
+    meteobase,
+    regis,
+    rws,
+    waterboard,
+    webservices,
+)
 from .regis import get_regis

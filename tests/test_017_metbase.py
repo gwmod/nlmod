8d6031f199e532cb42a7abdb27ad020ed3b51f0a
Martin Vonk <66305055+martinvonk@users.noreply.github.com>
2023-04-11 14:48:40 +0200

    Add Meteobase ASCII read function (#159)
    * add reader for meteobase zip files
    
    * add test for reading zipfile from meteobase
    
    * Update __init__.py
    
    add meteobase
    
    * Update meteobase.py
    
    fix | character
    
    * Update meteobase.py
    
    fix if statement
    
    * Update meteobase.py
    
    fix redefining built-in 'type'
    
    * fix test
    
    * Revert "fix test"
    
    This reverts commit e651df1d404e29dddf443bedfe81f32b08055da4.
    
    * fix test
    
    using os :))
    
    * Create Meteobase_ASCII_test.zip
    
    * Revert "fix test"
    
    This reverts commit cb4c60c79443756305aec3f97ee7942c41a035f5.
    
    * Update meteobase.py
    
    codacy fix
    
    * fix hour bug get_datetime_from_fname function
    
    only search name with removed date
    
    * rename test_015 to test_017
    
    * Update meteobase.py
    
    use zeros instead of empty
    
    * Replace np.datetime64 by pd.Timestamp
    
    ---------
    
    Co-authored-by: Ruben Caljé <r.calje@artesia-water.nl>

---
+++ tests/test_017_metbase.py
@@ -0,0 +1,8 @@
+import nlmod
+from pathlib import Path
+
+data_path = Path(__file__).parent / "data"
+
+
+def test_read_meteobase() -> None:
+    _ = nlmod.read.meteobase.read_meteobase(data_path / "Meteobase_ASCII_test.zip")

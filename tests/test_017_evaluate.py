6f15a0047d3dea308bf30d663b6ef9d528371c58
Ruben Caljé <r.calje@artesia-water.nl>
2023-04-11 10:23:14 +0200

    Remove some leftovers of evaluate

--- tests/test_017_evaluate.py
+++
@@ -1,8 +0,0 @@
-import nlmod
-import test_001_model
-
-
-def test_gxg():
-    ds = test_001_model.get_ds_from_cache("basic_sea_model")
-    head = nlmod.gwf.get_heads_da(ds)
-    nlmod.evaluate.calculate_gxg(head)

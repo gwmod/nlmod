bcddf30271c0397bb0811dcc1e7268e5fc4faf21
Ruben Caljé <r.calje@artesia-water.nl>
2023-04-14 14:17:31 +0200

    Add first methods to get data from knmi data platform

---
+++ nlmod/read/knmi_data_platform.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 14 10:53:17 2023
+
+@author: Ruben
+"""
+
+import os
+import requests
+import logging
+from tqdm import tqdm
+import xarray as xr
+
+
+logger = logging.getLogger(__name__)
+
+api_key = "eyJvcmciOiI1ZTU1NGUxOTI3NGE5NjAwMDEyYTNlYjEiLCJpZCI6IjI4ZWZlOTZkNDk2ZjQ3ZmE5YjMzNWY5NDU3NWQyMzViIiwiaCI6Im11cm11cjEyOCJ9"  # knmi
+# base_url = "https://api.dataplatform.knmi.nl/dataset-content/v1/datasets"
+base_url = "https://api.dataplatform.knmi.nl/open-data"
+
+
+def get_list_of_files(
+    dataset_name,
+    dataset_version,
+    max_keys=500,
+    start_after_filename=None,
+):
+    # Make sure to send the API key with every HTTP request
+    files = []
+    while True:
+        url = f"{base_url}/datasets/{dataset_name}/versions/{dataset_version}/files"
+        r = requests.get(url, headers={"Authorization": api_key})
+        params = {"maxKeys": f"{max_keys}"}
+        if start_after_filename is not None:
+            params["startAfterFilename"] = start_after_filename
+        r = requests.get(url, params=params, headers={"Authorization": api_key})
+        json = r.json()
+        files.extend([x["filename"] for x in json["files"]])
+        if json["isTruncated"]:
+            start_after_filename = files[-1]
+            print(start_after_filename)
+        else:
+            return files
+
+
+def download_file(dataset_name, dataset_version, filename, dirname="."):
+    url = f"{base_url}/datasets/{dataset_name}/versions/{dataset_version}/files/{filename}/url"
+    r = requests.get(url, headers={"Authorization": api_key})
+    if not os.path.isdir(dirname):
+        os.makedirs(dirname)
+    fname = os.path.join(dirname, filename)
+    try:
+        with requests.get(r.json()["temporaryDownloadUrl"], stream=True) as r:
+            r.raise_for_status()
+            with open(fname, "wb") as f:
+                for chunk in r.iter_content(chunk_size=8192):
+                    f.write(chunk)
+    except Exception:
+        logger.exception(f"Unable to download file {filename} using download URL")
+
+    logger.info(f"Successfully downloaded dataset file to {filename}")
+
+
+def read_dataset_from_zip(fname, hour=None):
+    from zipfile import ZipFile
+
+    with ZipFile(fname) as zipf:
+        data = []
+        for file in tqdm(zipf.namelist()):
+            if hour is not None:
+                if not file.endswith(f"{hour:02d}00.nc"):
+                    continue
+            else:
+                if not file.endswith(".nc"):
+                    continue
+            fo = zipf.open(file)
+            data.append(xr.open_dataset(fo))
+        ds = xr.concat(data, dim="time").sortby("time")
+    return ds

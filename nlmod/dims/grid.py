0f34283ffa71c2b5a394189bbb70e9a4280ce31c
Ruben Caljé <r.calje@artesia-water.nl>
2023-05-08 13:56:25 +0200

    Set gridgen files in a separate directory. Solves issue #158

--- nlmod/dims/grid.py
+++ nlmod/dims/grid.py
@@ -8,7 +8,7 @@
 """
 import logging
 import warnings
-
+import os
 import flopy
 import geopandas as gpd
 import numpy as np
@@ -267,7 +267,7 @@ def refine(
         exe_name = util.get_exe_path("gridgen")
 
     if model_ws is None:
-        model_ws = ds.model_ws
+        model_ws = os.path.join(ds.model_ws, "gridgen")
 
     if version.parse(flopy.__version__) < version.parse("3.3.6"):
         sim = flopy.mf6.MFSimulation()

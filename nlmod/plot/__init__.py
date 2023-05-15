70af375c43d697ee2db26ec5fb91e0c4ddd92890
dbrakenhoff <d.brakenhoff@artesia-water.nl>
2023-05-15 16:46:34 +0200

    fix init

--- nlmod/plot/__init__.py
+++ nlmod/plot/__init__.py
@@ -1,2 +1,19 @@
 from . import flopy
-from .plot import *
+from .plot import (
+    animate_map,
+    data_array,
+    facet_plot,
+    geotop_lithok_in_cross_section,
+    map_array,
+    modelgrid,
+    surface_water,
+)
+from .plotutil import (
+    add_background_map,
+    colorbar_inside,
+    get_figsize,
+    get_map,
+    rd_ticks,
+    rotate_yticklabels,
+    title_inside,
+)

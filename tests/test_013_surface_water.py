e11eacfac70d1bcc6983cd0c81330625aac9a73f
OnnoEbbens <onnoebbens@gmail.com>
2023-06-01 14:37:13 +0200

    add evaporation to test

--- tests/test_013_surface_water.py
+++ tests/test_013_surface_water.py
@@ -40,12 +40,15 @@ def test_gdf_lake():
     gwf = nlmod.gwf.gwf(ds, sim)
     nlmod.gwf.dis(ds, gwf)
 
+    ds['evap'] = (('time',), [0.0004])
+
     gdf_lake = gpd.GeoDataFrame(
         {
             "name": ["0", "0", "1"],
             "lakeno": [0, 0, 1],
             "strt": [1.0, 1.0, 2.0],
             "clake": [10.0, 10.0, 10.0],
+            'EVAPORATION': ['evap', 'evap', 'evap'],
             "lakeout": [1, 1, None],
             "outlet_invert": [0, 0, None],
         },
@@ -53,4 +56,5 @@ def test_gdf_lake():
     )
 
     # add lake to groundwaterflow model
-    nlmod.gwf.lake_from_gdf(gwf, gdf_lake, ds, boundname_column="name", recharge=False)
+    nlmod.gwf.lake_from_gdf(
+        gwf, gdf_lake, ds, boundname_column="name", recharge=False)

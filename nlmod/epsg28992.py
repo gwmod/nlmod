33d64785c9c75a9131b997514673a7d562b3acb1
Davíd Brakenhoff <d.brakenhoff@artesia-water.nl>
2023-08-24 17:48:15 +0200

    fix Projection issue in backgroundmaps #240 - add custom crs definition string

---
+++ nlmod/epsg28992.py
@@ -0,0 +1,7 @@
+# NOTE: this is the correct epsg:28992 definition for plotting backgroundmaps in RD
+EPSG_28992 = (
+    "+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 "
+    "+x_0=155000 +y_0=463000 +ellps=bessel "
+    "+towgs84=565.417,50.3319,465.552,-0.398957,0.343988,-1.8774,4.0725 +units=m "
+    "+no_defs"
+)

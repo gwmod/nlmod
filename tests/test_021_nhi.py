97bbc0aca8171f1507e82d898b386a71fdf4d7e7
Ruben Caljé <r.calje@artesia-water.nl>
2023-10-16 14:29:49 +0200

    Add nhi.py and test

---
+++ tests/test_021_nhi.py
@@ -0,0 +1,20 @@
+import os
+import numpy as np
+import tempfile
+import nlmod
+
+tmpdir = tempfile.gettempdir()
+
+
+def test_buidrainage():
+    model_ws = os.path.join(tmpdir, "buidrain")
+    ds = nlmod.get_ds([110_000, 130_000, 435_000, 445_000], model_ws=model_ws)
+    ds = nlmod.read.nhi.add_buisdrainage(ds)
+
+    # assert that all locations with a specified depth also have a positive conductance
+    mask = ~ds["buisdrain_depth"].isnull()
+    assert np.all(ds["buisdrain_cond"].data[mask] > 0)
+
+    # assert that all locations with a positive conductance also have a specified depth
+    mask = ds["buisdrain_cond"] > 0
+    assert np.all(~np.isnan(ds["buisdrain_depth"].data[mask]))

c2d3efc3c4b008be33d97c94ee31ad284a488ba6
OnnoEbbens <onnoebbens@gmail.com>
2023-10-20 09:27:22 +0200

    add more info on epsg 28892 definition, see comment Bas

--- nlmod/epsg28992.py
+++ nlmod/epsg28992.py
@@ -1,4 +1,10 @@
-# NOTE: this is the correct epsg:28992 definition for plotting backgroundmaps in RD
+"""
+NOTE: this is the correct epsg:28992 definition for plotting backgroundmaps in RD
+More info (in Dutch) here: 
+https://qgis.nl/2011/12/05/epsg28992-of-rijksdriehoekstelsel-verschuiving/
+This was still a problem in October 2023
+"""
+
 EPSG_28992 = (
     "+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 "
     "+x_0=155000 +y_0=463000 +ellps=bessel "

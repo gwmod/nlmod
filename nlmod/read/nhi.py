c60c2d657442e1fbfa39227012baafb7de817e61
Ruben Caljé <r.calje@artesia-water.nl>
2023-10-23 15:01:58 +0200

    Add docstrings to nhi

--- nlmod/read/nhi.py
+++ nlmod/read/nhi.py
@@ -10,7 +10,31 @@ from ..dims.resample import structured_da_to_ds
 logger = logging.getLogger(__name__)
 
 
-def download_file(url, pathname, filename=None, overwrite=False, timeout=120):
+def download_file(url, pathname, filename=None, overwrite=False, timeout=120.0):
+    """
+    Download a file from the NHI website.
+
+    Parameters
+    ----------
+    url : str
+        The url of the file to download.
+    pathname : str
+        The pathname to which the file is downloaded.
+    filename : str, optional
+        The name of the file to contain the downloadded data. When filename is None, it
+        is derived from url. The default is None.
+    overwrite : bool, optional
+        Overwrite the file if it allready exists. The default is False.
+    timeout : float, optional
+        How many seconds to wait for the server to send data before giving up. The
+        default is 120.
+
+    Returns
+    -------
+    fname : str
+        The full path of the downloaded file.
+
+    """
     if filename is None:
         filename = url.split("/")[-1]
     fname = os.path.join(pathname, filename)
@@ -23,6 +47,24 @@ def download_file(url, pathname, filename=None, overwrite=False, timeout=120):
 
 
 def download_buisdrainage(pathname, overwrite=False):
+    """
+    Download resistance and depth of buisdrainage from the NHI website
+
+    Parameters
+    ----------
+    pathname : str
+        The pathname to which the files are downloaded.
+    overwrite : bool, optional
+        Overwrite the files if they allready exists. The default is False.
+
+    Returns
+    -------
+    fname_c : str
+        The full path of the downloaded file containing the resistance of buisdrainage.
+    fname_d : str
+        The full path of the downloaded file containing the depth of buisdrainage.
+
+    """
     url_bas = "https://thredds.data.nhi.nu/thredds/fileServer/opendap/models/nhi3_2/25m"
 
     # download resistance
@@ -44,6 +86,47 @@ def add_buisdrainage(
     cond_method="average",
     depth_method="mode",
 ):
+    """
+    Add data about the buisdrainage to the model Dataset.
+
+    This data consists of the conductance of buisdrainage (m2/d) and the depth of
+    buisdrainage (m to surface level). With the default settings for `cond_method` and
+    `depth_method`, the conductance is calculated from the area weighted average of the
+    resistance in a cell, while the depth is set to the value which appears most often
+    in a cell. Cells without data (or 0 in the case of the resistance) are ignored in
+    these calculations.
+
+    The original data of the resistance and the depth of buisdrainage at a 25x25 m scale
+    is downloaded to `pathname` when not found.
+
+    Parameters
+    ----------
+    ds : xr.Dataset
+        The model Dataset.
+    pathname : str, optional
+        The pathname containing the downloaded files or the pathname to which the files
+        are downloaded. When pathname is None, it is set the the cachedir. The default
+        is None.
+    cond_var : str, optional
+        The name of the variable in ds to contain the data about the conductance of
+        buisdrainage. The default is "buisdrain_cond".
+    depth_var : str, optional
+        The name of the variable in ds to contain the data about the depth of
+        buisdrainage. The default is "buisdrain_depth".
+    cond_method : str, optional
+        The method to transform the conductance of buisdrainage to the model Dataset.
+        The default is "average".
+    depth_method : str, optional
+        The method to transform the depth of buisdrainage to the model Dataset. The
+        default is "mode".
+
+    Returns
+    -------
+    ds : xr.Dataset
+        The model dataset with added variables with the names `cond_var` and
+        `depth_var`.
+
+    """
     if pathname is None:
         pathname = ds.cachedir
     # download files if needed
@@ -54,7 +137,7 @@ def add_buisdrainage(
         ds = ds.rio.write_crs(28992)
 
     # use cond_methd for conductance
-    # (default is "average" to account for locations without pipe drainage, where the 
+    # (default is "average" to account for locations without pipe drainage, where the
     # conductance is 0)
     buisdrain_c = rioxarray.open_rasterio(fname_c, mask_and_scale=True)[0]
     # calculate a conductance (per m2) from a resistance

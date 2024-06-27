821688c359e8eb431d66583bf7b83465b91d1106
Davíd Brakenhoff <d.brakenhoff@artesia-water.nl>
2024-06-27 14:19:03 +0200

    restore the position of NLMOD_DATADIR

--- nlmod/__init__.py
+++ nlmod/__init__.py
@@ -1,9 +1,10 @@
-# ruff: noqa: F401
+# ruff: noqa: F401 E402
 import os
+
+NLMOD_DATADIR = os.path.join(os.path.dirname(__file__), "data")
 
 from . import dims, gis, gwf, gwt, modpath, plot, read, sim, util
 from .dims import base, get_ds, grid, layers, resample, time, to_model_ds
 from .util import download_mfbinaries
 from .version import __version__, show_versions
 
-NLMOD_DATADIR = os.path.join(os.path.dirname(__file__), "data")

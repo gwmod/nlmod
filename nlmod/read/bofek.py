689230d4b876f007cfb33292f2a570b803d17b26
Ruben Caljé <r.calje@artesia-water.nl>
2025-01-15 21:08:22 +0100

    black formatting of bofek.py

--- nlmod/read/bofek.py
+++ nlmod/read/bofek.py
@@ -14,7 +14,7 @@ logger = logging.getLogger(__name__)
 @cache.cache_pickle
 def get_gdf_bofek(ds=None, extent=None, timeout=3600):
     """Get geodataframe of bofek 2020 wihtin the extent of the model.
-    
+
     It does so by downloading a zip file (> 100 MB) and extracting the relevant
     geodatabase. Therefore the function can be slow, ~35 seconds depending on your
     internet connection.
@@ -42,21 +42,21 @@ def get_gdf_bofek(ds=None, extent=None, timeout=3600):
     # set paths
     tmpdir = Path(NLMOD_DATADIR)
 
-    fname_7z = tmpdir / 'BOFEK2020_GIS.7z'
-    fname_bofek = tmpdir / 'GIS' / 'BOFEK2020_bestanden' / 'BOFEK2020.gdb'
-    fname_bofek_geojson = tmpdir / 'bofek' / 'BOFEK2020.geojson'
-    bofek_zip_url = 'https://www.wur.nl/nl/show/bofek-2020-gis-1.htm'
+    fname_7z = tmpdir / "BOFEK2020_GIS.7z"
+    fname_bofek = tmpdir / "GIS" / "BOFEK2020_bestanden" / "BOFEK2020.gdb"
+    fname_bofek_geojson = tmpdir / "bofek" / "BOFEK2020.geojson"
+    bofek_zip_url = "https://www.wur.nl/nl/show/bofek-2020-gis-1.htm"
 
     if not fname_bofek_geojson.exists():
         # download zip
-        logger.info('Downloading BOFEK2020 GIS data (~35 seconds)')
+        logger.info("Downloading BOFEK2020 GIS data (~35 seconds)")
         r = requests.get(bofek_zip_url, timeout=timeout)
-    
-        logger.debug('Extracting zipped BOFEK2020 GIS data')
+
+        logger.debug("Extracting zipped BOFEK2020 GIS data")
         with zipfile.ZipFile(io.BytesIO(r.content)) as z:
             # extract 7z
             z.extractall(tmpdir)
-            
+
         with py7zr.SevenZipFile(fname_7z, mode="r") as z:
             z.extract(
                 targets=["GIS/BOFEK2020_bestanden/BOFEK2020.gdb"],
@@ -65,21 +65,21 @@ def get_gdf_bofek(ds=None, extent=None, timeout=3600):
             )
 
         # clean up
-        logger.debug('Remove zip files')
+        logger.debug("Remove zip files")
         Path(fname_7z).unlink()
 
         # read geodatabase
-        logger.debug('convert geodatabase to geojson')
+        logger.debug("convert geodatabase to geojson")
         gdf_bofek = gpd.read_file(fname_bofek)
 
         # save to geojson
-        gdf_bofek.to_file(fname_bofek_geojson, driver='GeoJSON')
+        gdf_bofek.to_file(fname_bofek_geojson, driver="GeoJSON")
 
         # remove geodatabase
         shutil.rmtree(fname_bofek)
 
     # read geojson
-    logger.debug(f'read bofek2020 geojson from {fname_bofek_geojson}')
+    logger.debug(f"read bofek2020 geojson from {fname_bofek_geojson}")
     gdf_bofek = gpd.read_file(fname_bofek_geojson)
 
     if extent is not None:

8fc947f1d5382e08d7ddfb80a6267a171ddda815
Onno Ebbens <onnoebbens@gmail.com>
2025-02-12 12:24:57 +0100

    Update test

--- tests/test_030_brt.py
+++ tests/test_030_brt.py
@@ -1,5 +1,8 @@
 import nlmod
+import geopandas as gpd
 
 def test_brt():
     extent = [119900, 120000, 440000, 440100]
-    brt = nlmod.read.brt.get_brt_v2(extent)
+    brt = nlmod.read.brt.get_brt(extent)
+
+    assert isinstance(brt, gpd.GeoDataFrame)

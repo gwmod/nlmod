2cf39be04a8e1d0f8fedc5f436306696b58c98b5
Davíd Brakenhoff <d.brakenhoff@artesia-water.nl>
2025-02-26 17:41:12 +0100

    compute area if it does not exist

--- nlmod/dims/resample.py
+++ nlmod/dims/resample.py
@@ -541,7 +541,7 @@ def structured_da_to_ds(da, ds, method="average", nodata=np.nan):
         da_out = get_da_from_da_ds(ds, dims=tuple(dims), data=nodata)
         from .grid import get_affine
 
-        for area in np.unique(ds["area"]):
+        for area in np.unique(ds["area"] if "area" in ds else get_area(ds)):
             dx = dy = np.sqrt(area)
             x, y = get_xy_mid_structured(ds.extent, dx, dy)
             da_temp = da.rio.reproject(

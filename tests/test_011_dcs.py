8fdc42adce7935af7f86a04e2d025102ab74394f
Ruben Caljé <r.calje@artesia-water.nl>
2025-04-15 10:30:43 +0200

    Calculate grid edges differently in DatasetCrossSection

--- tests/test_011_dcs.py
+++ tests/test_011_dcs.py
@@ -7,6 +7,18 @@ import nlmod
 def test_dcs_structured():
     ds = util.get_ds_structured()
     line = [(0, 0), (1000, 1000)]
+    plt.subplots()
+    dcs = nlmod.plot.DatasetCrossSection(ds, line)
+    dcs.plot_layers()
+    dcs.label_layers()
+    dcs.plot_array(ds["kh"], alpha=0.5)
+    dcs.plot_grid()
+
+
+def test_dcs_cs_model():
+    ds = util.get_ds_structured([0, 1000, 0, 100], delr=100)
+    line = [(0, 0), (1000, 100)]
+    plt.subplots()
     dcs = nlmod.plot.DatasetCrossSection(ds, line)
     dcs.plot_layers()
     dcs.label_layers()
@@ -17,6 +29,7 @@ def test_dcs_structured():
 def test_dcs_vertex():
     ds = util.get_ds_vertex()
     line = [(0, 0), (1000, 1000)]
+    plt.subplots()
     dcs = nlmod.plot.DatasetCrossSection(ds, line)
     dcs.plot_layers()
     dcs.label_layers()

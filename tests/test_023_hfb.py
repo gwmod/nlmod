e7cdb5fe02a47ea1f11a173660349a34d4f9eb26
Davíd Brakenhoff <d.brakenhoff@artesia-water.nl>
2025-05-14 17:16:47 +0200

    update hfb tests

--- tests/test_023_hfb.py
+++ tests/test_023_hfb.py
@@ -16,12 +16,12 @@ def test_get_hfb_spd():
     coords = [(0, 1000), (1000, 0)]
     gdf = gpd.GeoDataFrame({"geometry": [LineString(coords)]})
 
-    spd = nlmod.gwf.horizontal_flow_barrier.get_hfb_spd(gwf, gdf, depth=5.0)
+    spd = nlmod.gwf.hfb.get_hfb_spd(ds, gdf, depth=5.0)
     hfb = flopy.mf6.ModflowGwfhfb(gwf, stress_period_data={0: spd})
 
     # also test the plot method
     ax = gdf.plot()
-    nlmod.gwf.horizontal_flow_barrier.plot_hfb(hfb, gwf, ax=ax)
+    nlmod.gwf.hfb.plot_hfb(hfb, gwf, ax=ax)
 
 
 def test_polygon_to_hfb():
@@ -32,8 +32,8 @@ def test_polygon_to_hfb():
     coords = [(135, 230), (568, 170), (778, 670), (260, 786)]
     gdf = gpd.GeoDataFrame({"geometry": [Polygon(coords)]}).reset_index()
 
-    hfb = nlmod.gwf.horizontal_flow_barrier.polygon_to_hfb(gdf, ds, gwf=gwf)
+    hfb = nlmod.gwf.hfb.polygon_to_hfb(gdf, ds, gwf=gwf)
 
     # also test the plot method
     ax = gdf.plot()
-    nlmod.gwf.horizontal_flow_barrier.plot_hfb(hfb, gwf, ax=ax)
+    nlmod.gwf.hfb.plot_hfb(hfb, gwf, ax=ax)

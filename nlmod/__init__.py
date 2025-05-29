c07ba341d0cfa9680160cb8eee71ade791293d48
Davíd Brakenhoff <d.brakenhoff@artesia-water.nl>
2025-05-29 23:33:47 +0200

    update init

--- nlmod/__init__.py
+++ nlmod/__init__.py
@@ -3,7 +3,20 @@ import os
 
 NLMOD_DATADIR = os.path.join(os.path.dirname(__file__), "data")
 
-from . import config, dims, gis, gwf, gwt, modpath, observations, plot, prt, read, sim, util
+from . import (
+    config,
+    dims,
+    gis,
+    gwf,
+    gwt,
+    modpath,
+    observations,
+    plot,
+    prt,
+    read,
+    sim,
+    util,
+)
 from .dims import base, get_ds, grid, layers, resample, time, to_model_ds
 from .util import download_mfbinaries
 from .version import __version__, show_versions
